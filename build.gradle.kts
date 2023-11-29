d90ec5b6596ecbe1b88d0ea754ac1dd397d952e8
jemma <jemma.wells@johnlewis.co.uk>
2023-11-29 12:28:11 +0000

    Initial commit with gradle stuff

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks {
+    named<Test>("test") {
+        useJUnitPlatform()
+    }
+}
